0f13bb76bccdec01d40168fa3b779076a93e501d
Artsiom <stinny.by@gmail.com>
2025-06-03 06:46:53 +0200

    Fixed clear.sql

--- database/clear.sql
+++ database/clear.sql
@@ -116,3 +116,7 @@ drop function if exists check_unique_active_team() cascade;
 
 drop function if exists check_unique_active_vacation() cascade;
 
+drop function if exists add_employee(varchar, varchar, varchar, char, varchar, varchar, varchar, char, integer, integer,
+                                     date, varchar, integer, date, date, varchar, date, date) cascade;
+
+
