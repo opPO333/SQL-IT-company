a9fddde7f1e1870053d1b170df7481bfb479c009
Artsiom <stinny.by@gmail.com>
2025-06-08 22:52:50 +0200

    Added inserts

--- database/clear.sql
+++ database/clear.sql
@@ -1,40 +1,3 @@
-DROP TYPE IF EXISTS equipment_type CASCADE;
-DROP TYPE IF EXISTS equipment_status CASCADE;
-
-DROP VIEW IF EXISTS employees_view;
-
-DROP TRIGGER IF EXISTS pesel_check ON employees CASCADE;
-DROP FUNCTION IF EXISTS pesel_check CASCADE;
-
-DROP TABLE IF EXISTS
-    schedule_exception_types,
-    employee_schedule,
-    employee_departments_history,
-    employee_positions_history,
-    employee_teams_history,
-    departments,
-    addresses,
-    cities,
-    regions,
-    countries,
-    schedule_exceptions,
-    position_schedules,
-    employee_days,
-    vacations,
-    tasks,
-    teams,
-    projects,
-    employees,
-    positions,
-    employee_name_history,
-    head_departments_history,
-    equipment,
-    equipment_status_history,
-    employee_equipment_history
-CASCADE;
-
-
-
 drop table if exists employee_name_history cascade;
 
 drop table if exists head_departments_history cascade;
@@ -48,8 +11,6 @@ drop table if exists employee_teams_history cascade;
 drop table if exists employee_schedule cascade;
 
 drop table if exists employee_salary cascade;
-
-drop table if exists employer_schedule cascade;
 
 drop table if exists schedule_exceptions cascade;
 
@@ -87,11 +48,13 @@ drop table if exists equipment cascade;
 
 drop type if exists equipment_type cascade;
 
+drop view if exists teams_view cascade;
+
 drop view if exists employees_view cascade;
 
 drop view if exists departments_view cascade;
 
-drop view if exists teams_view cascade;
+drop view if exists employee_departments_history_view cascade;
 
 drop function if exists pesel_check() cascade;
 
@@ -99,12 +62,10 @@ drop function if exists employee_name_change() cascade;
 
 drop function if exists check_and_close_position() cascade;
 
-drop function if exists check_single_active_position() cascade;
-
 drop function if exists check_employee_inserted_correctly() cascade;
 
-drop function if exists add_employee(varchar, varchar, varchar, char, varchar, varchar, varchar, char, integer, integer,
-                                     date, varchar, date, varchar, date, varchar, date, date) cascade;
+drop function if exists add_employee(varchar, varchar, varchar, char, varchar, varchar, varchar, char, integer, date,
+                                     varchar, integer, integer, date, date, varchar, date, date) cascade;
 
 drop function if exists check_departments_consistency() cascade;
 
@@ -120,6 +81,9 @@ drop function if exists check_unique_active_team() cascade;
 
 drop function if exists check_unique_active_vacation() cascade;
 
-drop function if exists add_employee(varchar, varchar, varchar, char, varchar, varchar, varchar, char, integer, integer,
-                                     date, varchar, integer, integer, date, date, varchar, date, date) cascade;
+drop function if exists employees_view_insert_tr() cascade;
+
+drop function if exists trg_employees_view_update() cascade;
+
+drop function if exists manage_employees_view_delete() cascade;
 
